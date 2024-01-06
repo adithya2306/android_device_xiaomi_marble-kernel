bbf10dcf747298e0a8d29c990ab8fe7531019cb8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-06 18:45:35 +0530

    marble-kernel: Improve kernel extraction script
    - Allow skipping kernel image
    - Use AOSP unpack script
    - Print each file being copied
    - Misc refinements

--- extract-files.sh
+++ extract-files.sh
@@ -8,73 +8,91 @@
 set -e
 
 ### Setup
-DUMP="${1}"
+DUMP=
 MY_DIR="${BASH_SOURCE%/*}"
+SRC_ROOT="${MY_DIR}/../../.."
+TMP_DIR=$(mktemp -d)
+EXTRACT_KERNEL=true
 declare -a MODULE_FOLDERS=("vendor_ramdisk" "vendor_dlkm" "system_dlkm")
 
-if [ ! -f "${MY_DIR}/Module.symvers" ]; then
-    touch "${MY_DIR}/Module.symvers"
-fi
-if [ ! -f "${MY_DIR}/System.map" ]; then
-    touch "${MY_DIR}/System.map"
-fi
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-kernel )
+                EXTRACT_KERNEL=false
+                ;;
+        * )
+                DUMP="${1}"
+                ;;
+    esac
+    shift
+done
+
+[ -f "${MY_DIR}/Module.symvers" ] || touch "${MY_DIR}/Module.symvers"
+[ -f "${MY_DIR}/System.map" ] || touch "${MY_DIR}/System.map"
 
 # Check if dump is specified and exists
-if [ "$#" -ne 1 ]; then
-    echo "Please specify your dump"
+if [ -z "${DUMP}" ]; then
+    echo "Please specify the dump!"
     exit 1
-fi
-if [ ! -d "${DUMP}" ]; then
-    echo "Unable to find dump at ${DUMP}"
+elif [ ! -d "${DUMP}" ]; then
+    echo "Unable to find dump at ${DUMP}!"
     exit 1
 fi
 
+echo "Extracting files from ${DUMP}:"
+
 ### Kernel
-cp -f "${DUMP}/aosp-device-tree/prebuilts/kernel" "${MY_DIR}/Image"
+if ${EXTRACT_KERNEL}; then
+    echo "Extracting boot image.."
+    ${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py \
+        --boot_img "${DUMP}/boot.img" \
+        --out "${TMP_DIR}/boot.out" > /dev/null
+    cp -f "${TMP_DIR}/boot.out/kernel" ${MY_DIR}/Image
+    echo "  - Image"
+fi
 
 ### DTBS
 # Cleanup / Preparation
-if [ ! -d "${MY_DIR}/dtbs" ]; then
-    mkdir "${MY_DIR}/dtbs"
-fi
+rm -rf "${MY_DIR}/dtbs"
+mkdir "${MY_DIR}/dtbs"
 
-rm -f "${MY_DIR}/dtbs/00_kernel"
-find "${MY_DIR}/dtbs" -type f -name "*.dtb" -delete
-rm -f "${MY_DIR}/dtbs/dtbo.img"
+echo "Extracting vendor_boot image..."
+${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py \
+    --boot_img "${DUMP}/vendor_boot.img" \
+    --out "${TMP_DIR}/vendor_boot.out" > /dev/null
+
+curl -sSL "https://raw.githubusercontent.com/PabloCastellano/extract-dtb/master/extract_dtb/extract_dtb.py" > ${TMP_DIR}/extract_dtb.py
 
 # Copy
-cp -f "${DUMP}/aosp-device-tree/prebuilts/dtbo.img" "${MY_DIR}/dtbs/dtbo.img"
-
-mkdir "${MY_DIR}/_temp"
-curl -L "https://raw.githubusercontent.com/PabloCastellano/extract-dtb/master/extract_dtb/extract_dtb.py" > ${MY_DIR}/_temp/extract_dtb.py
-cp -f "${DUMP}/aosp-device-tree/prebuilts/dtb.img" "${MY_DIR}/_temp/dtb.img"
-python3 "${MY_DIR}/_temp/extract_dtb.py" "${MY_DIR}/_temp/dtb.img" -o "${MY_DIR}/dtbs"
-rm -rf "_temp"
+python3 "${TMP_DIR}/extract_dtb.py" "${TMP_DIR}/vendor_boot.out/dtb" -o "${TMP_DIR}/dtbs" > /dev/null
+find "${TMP_DIR}/dtbs" -type f -name "*.dtb" \
+    -exec cp {} "${MY_DIR}/dtbs" \; \
+    -exec printf "  - dtbs/" \; \
+    -exec basename {} \;
+cp -f "${DUMP}/dtbo.img" "${MY_DIR}/dtbs/dtbo.img"
+echo "  - dtbs/dtbo.img"
 
 ### Modules
 # Cleanup / Preparation
 for MODULE_FOLDER in "${MODULE_FOLDERS[@]}"; do
-    if [ -d "${MY_DIR}/${MODULE_FOLDER}" ]; then
-        find "${MY_DIR}/${MODULE_FOLDER}" -type f -name "*.ko" -maxdepth 1 -delete
-        find "${MY_DIR}/${MODULE_FOLDER}" -type f -name "*modules*" -maxdepth 1 -delete
-    elif [ ! -d "${MY_DIR}/${MODULE_FOLDER}" ] && [ "${MODULE_FOLDER}" != "." ]; then
-        mkdir "${MY_DIR}/${MODULE_FOLDER}"
-    fi
+    rm -rf "${MY_DIR}/${MODULE_FOLDER}"
+    mkdir "${MY_DIR}/${MODULE_FOLDER}"
 done
 
 # Copy
 for MODULE_FOLDER in "${MODULE_FOLDERS[@]}"; do
+    MODULE_SRC="${DUMP}/${MODULE_FOLDER}"
     if [ "${MODULE_FOLDER}" == "vendor_ramdisk" ]; then
-        mkdir "${MY_DIR}/_temp"
-        curl -L "https://github.com/cfig/Android_boot_image_editor/releases/download/v13_r3/boot_editor_v13r3.zip" > ${MY_DIR}/_temp/boot_editor_v13r3.zip
-        unzip "${MY_DIR}/_temp/boot_editor_v13r3.zip" -d "${MY_DIR}/_temp/"
-        cp -f "${DUMP}/vendor_boot.img" "${MY_DIR}/_temp/boot_editor_v13r3/"
-        ( cd "${MY_DIR}/_temp/boot_editor_v13r3/" && ./gradlew unpack )
-        find "${MY_DIR}/_temp/boot_editor_v13r3/build/unzip_boot/root.1/" -type f -name "*.ko" -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \;
-        find "${MY_DIR}/_temp/boot_editor_v13r3/build/unzip_boot/root.1/" -type f -name "*modules*" -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \;
-        rm -rf "_temp"
-    else
-        find "${DUMP}/${MODULE_FOLDER}/" -type f -name "*.ko" -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \;
-        find "${DUMP}/${MODULE_FOLDER}/" -type f -name "*modules*" -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \;
+        lz4 -qd "${TMP_DIR}/vendor_boot.out/vendor_ramdisk00" "${TMP_DIR}/vendor_ramdisk.cpio"
+        7z x "${TMP_DIR}/vendor_ramdisk.cpio" -o"${TMP_DIR}/vendor_ramdisk" > /dev/null
+        MODULE_SRC="${TMP_DIR}/vendor_ramdisk"
     fi
+    [ -d "${MODULE_SRC}" ] || break
+    find "${MODULE_SRC}/lib/modules" -type f \
+        -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \; \
+        -exec printf "  - ${MODULE_FOLDER}/" \; \
+        -exec basename {} \;
 done
+
+# Clear temp dir
+rm -rf "${TMP_DIR}"
